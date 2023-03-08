63d0b470be51785e5a1ca8db4a9f77131e6a5b1d
Kashira2 <nekit.egorov@icloud.com>
2023-03-08 06:30:27 +0300

    end

--- dz4.java
+++ dz4.java
@@ -15,39 +15,41 @@ public class dz4 {
     public static void main(String[] args) {
         try {
             Scanner reader = new Scanner(System.in);
-            System.out.printf("Введите уравнение, соблюдая пробелы по типу (23 + 23 = 46) или (2? + 2? = 46): \n");
+            System.out.printf("Введите уравнение, соблюдая пробелы по типу (2? + 2? = 46): \n");
             String str = reader.nextLine();
             reader.close();
+            if (str.charAt(1) == '?' || str.charAt(0) == '?') {
+                int count = 0;
 
-            int count = 0;
+                for (int i = 0; i < 10; i++) {
+                    char j = (char) (i + '0');
+                    String num1 = str.replace('?', j);
 
-            for (int i = 0; i < 10; i++) {
-                char j = (char) (i + '0');
-                String num1 = str.replace('?', j);
+                    String[] nums = num1.split(" ");
 
-                String[] nums = num1.split(" ");
+                    int one = Integer.parseInt(nums[0]);
+                    int two = Integer.parseInt(nums[2]);
+                    int three = Integer.parseInt(nums[4]);
+                    char sign = nums[1].charAt(0);
 
-                int one = Integer.parseInt(nums[0]);
-                int two = Integer.parseInt(nums[2]);
-                int three = Integer.parseInt(nums[4]);
-                char sign = nums[1].charAt(0);
-
-                if (sign == '+') {
-                    if (one + two == three) {
-                        System.out.println("Возможные варианты уравнения: ");
-                        System.out.println(num1);
-                        count++;
-                    }
-                } else if (sign == '-') {
-                    if (one - two == three) {
-                        System.out.println("Возможные варианты уравнения: ");
-                        System.out.println(num1);
-                        count++;
+                    if (sign == '+') {
+                        if (one + two == three) {
+                            System.out.println("Возможный вариант уравнения: ");
+                            System.out.println(num1);
+                            count++;
+                        }
+                    } else if (sign == '-') {
+                        if (one - two == three) {
+                            System.out.println("Возможные варианты уравнения: ");
+                            System.out.println(num1);
+                            count++;
+                        }
                     }
                 }
+                if (count < 1)
+                    System.out.println("Варианты не найдены");
             }
-            if (count < 1)
-                System.out.println("Варианты не найдены");
+            else System.out.println("Уравнение не требует поиска вариантов замены '?' на цифру ");
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
